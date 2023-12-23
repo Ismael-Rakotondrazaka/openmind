af39582202bb0fd7333fb18ec7af8d59e32c4527
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-12-23 14:43:04 +0300

    feat: :tada: Setup Nuxt project

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  build: {
+    transpile: [
+      'trpc-nuxt'
+    ]
+  },
+  modules: [
+    '@sidebase/nuxt-auth'
+  ],
+  typescript: {
+    shim: false
+  }
+})
