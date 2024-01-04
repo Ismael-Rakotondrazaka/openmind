8640a28ff50ef202bc33bfec903e3cc53e786053
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-01-04 11:16:17 +0300

    feat: :sparkles: Create update-article schema for server side.

---
+++ server/utils/requests/articles/updateArticle.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+import { JSDOM } from "jsdom";
+import {
+  countHtmlAsTextLength,
+  articleConfig,
+  updateArticleBodyBaseSchema,
+} from "~/utils";
+
+export const updateArticleBodySchema = updateArticleBodyBaseSchema.and(
+  z
+    .object({
+      content: z
+        .string()
+        .trim()
+        .superRefine((value: string, ctx) => {
+          const dom = new JSDOM(value);
+
+          const length: number = countHtmlAsTextLength(
+            dom.window.document.body,
+          );
+
+          if (length < articleConfig.CONTENT_MIN_LENGTH) {
+            ctx.addIssue({
+              code: z.ZodIssueCode.too_small,
+              minimum: articleConfig.CONTENT_MIN_LENGTH,
+              inclusive: true,
+              fatal: true,
+              type: "string",
+            });
+
+            return z.NEVER;
+          }
+
+          if (length > articleConfig.CONTENT_MAX_LENGTH) {
+            ctx.addIssue({
+              code: z.ZodIssueCode.too_big,
+              fatal: true,
+              maximum: articleConfig.CONTENT_MAX_LENGTH,
+              inclusive: true,
+              type: "string",
+            });
+
+            return z.NEVER;
+          }
+        }),
+    })
+    .partial(),
+);
