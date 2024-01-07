41508e9fb9a1d913b397a29eaa2c929812a292bf
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-01-07 10:50:19 +0300

    feat: :sparkles: Add pagination to index-article response data schema.

--- utils/requests/articles/indexArticle.ts
+++ utils/requests/articles/indexArticle.ts
@@ -1,4 +1,3 @@
-import type { Article, User } from "@prisma/client";
 import { z } from "zod";
 import { makePageSizeSchema, PageSchema } from "../../models/paginations";
 import {
@@ -27,21 +26,19 @@ export type IndexArticleQuery = z.infer<typeof IndexArticleQuerySchema>;
 /*                             Index article data                             */
 /* -------------------------------------------------------------------------- */
 
-export const IndexArticleDataSchema = z.object({
-  articles: z.array(
-    ArticleSchema.and(
-      z.object({
-        user: UserSchema,
-      }),
+export const IndexArticleDataSchema = z
+  .object({
+    articles: z.array(
+      ArticleSchema.and(
+        z.object({
+          user: UserSchema,
+        }),
+      ),
     ),
-  ),
-});
+  })
+  .merge(PaginationSchema);
 
-export type IndexArticleData = {
-  articles: (Article & {
-    user: Omit<User, "password" | "email" | "emailVerifiedAt">;
-  })[];
-};
+export type IndexArticleData = z.infer<typeof IndexArticleDataSchema>;
 
 /* -------------------------------------------------------------------------- */
 /*                             Index article error                            */
