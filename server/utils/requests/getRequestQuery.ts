dae50eb32ef20448d9df5bb46c2018caa8380a7d
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-01-08 06:53:49 +0300

    refactor: :recycle: Use helper to parse nested JSON.

--- server/utils/requests/getRequestQuery.ts
+++ server/utils/requests/getRequestQuery.ts
@@ -1,16 +1,10 @@
 import type { H3Event } from "h3";
-import { destr } from "destr";
+import { parseNestedJSON } from "~/utils";
 
 export const getRequestQuery = (event: H3Event): Record<string, unknown> => {
   const query = getQuery(event);
 
-  const nestedParsed: Record<string, unknown> = {};
-
-  for (const key in query) {
-    if (Object.prototype.hasOwnProperty.call(query, key)) {
-      nestedParsed[key] = destr(query[key]);
-    }
-  }
+  const nestedParsed: Record<string, unknown> = parseNestedJSON(query);
 
   return nestedParsed;
 };
