bb10a9a3fd006d974c91d1313916a2da640b4c0b
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-01-08 18:44:01 +0300

    refactor: :recycle: Remove comments and refactor code in countHtmlAsTextLength.

--- utils/strings/countHtmlAsText.ts
+++ utils/strings/countHtmlAsText.ts
@@ -1,10 +1,16 @@
 export const countHtmlAsTextLength = (element: HTMLElement): number => {
   let textLength = 0;
 
+  // ! Node is not accessible in backend
+  // Node.TEXT_NODE
+  const nodeTextNode: number = 3;
+  // Node.ELEMENT_NODE;
+  const nodeElementNode: number = 1;
+
   const countText = (node: Node): void => {
-    if (node.nodeType === /* Node.TEXT_NODE */ 3) {
+    if (node.nodeType === nodeTextNode) {
       textLength += (node.textContent || "").length;
-    } else if (node.nodeType === /* Node.ELEMENT_NODE */ 1) {
+    } else if (node.nodeType === nodeElementNode) {
       const tagName = (node as Element).tagName.toLowerCase();
 
       const atomicElements = [
@@ -31,11 +37,3 @@ export const countHtmlAsTextLength = (element: HTMLElement): number => {
   countText(element);
   return textLength;
 };
-
-// Example usage:
-/* const htmlString = "<div><p>here</p><img src=\"/emoji.jpg\" />hello world</div><p>cool<br /><span>glass</span></p>";
-const parser = new JSDOM(htmlString);
-// const doc = parser.parseFromString(htmlString, "text/html");
-
-const result = countHtmlAsTextLength(parser.window.document.body);
-console.log(result); // Output: 26 */
