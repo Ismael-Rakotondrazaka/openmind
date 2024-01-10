f42e24ddfa833f2a6eaf0bc9eb616191fc1c950d
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-01-10 18:22:48 +0300

    feat: :sparkles: Add a custom boolean schema.

--- utils/schemas/types/booleans.ts
+++ utils/schemas/types/booleans.ts
@@ -13,3 +13,22 @@ export const NestedBoolFilterSchema: z.ZodType<Prisma.NestedBoolFilter> =
       .union([z.boolean(), z.lazy(() => NestedBoolFilterSchema)])
       .optional(),
   });
+
+export const CustomBooleanSchema = z.union([
+  z.boolean(),
+  z.string().transform((val: string, ctx): boolean => {
+    if (val === "true") {
+      return true;
+    } else if (val === "false") {
+      return false;
+    } else {
+      ctx.addIssue({
+        code: z.ZodIssueCode.invalid_enum_value,
+        received: val,
+        options: ["true", "false"],
+      });
+
+      return z.NEVER;
+    }
+  }),
+]);
