8d7ff738931e0fb27a6e4c7ed592807ee7b1793c
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-01-10 20:08:51 +0300

    fix: :bug: Add missing imports in update-article event handler.

--- server/api/articles/[slug]/index.put.ts
+++ server/api/articles/[slug]/index.put.ts
@@ -7,6 +7,8 @@ import {
   type UpdateArticleParam,
   createBadRequestError,
   createUnauthorizedError,
+  createNotFoundError,
+  createForbiddenError,
   getRequestErrorMessage,
   UpdateArticleParamSchema,
 } from "~/utils";
