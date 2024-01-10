7636861cd984e0d0141b5c339fbe319d72af6f0a
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-01-10 18:04:07 +0300

    feat: :sparkles: Create file schemas.

---
+++ server/utils/schemas/files/index.ts
@@ -0,0 +1,2 @@
+export * from "./fileSchema";
+export * from "./makeSafeFileSchema";
---
+++ server/utils/schemas/files/makeSafeFileSchema.ts
@@ -0,0 +1,67 @@
+import { fileTypeFromBuffer, type FileTypeResult } from "file-type";
+import { z } from "zod";
+import { FileSchema } from "./fileSchema";
+
+export type UploadedFile = {
+  file: File;
+  buffer: Buffer;
+};
+
+export const makeSafeFileSchema = (maxSize: number, mimeTypes: string[]) =>
+  FileSchema.transform(async (val: File, ctx): Promise<UploadedFile> => {
+    const arrayBuffer: ArrayBuffer = await val.arrayBuffer();
+
+    const fileTypeResult: FileTypeResult | undefined =
+      await fileTypeFromBuffer(arrayBuffer);
+
+    if (fileTypeResult === undefined) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: `File not supported. Expected formats: ${mimeTypes
+          .map((val: string) => `"${val}"`)
+          .join(", ")}.`,
+        fatal: true,
+      });
+
+      return z.NEVER;
+    }
+
+    if (!mimeTypes.includes(fileTypeResult.mime)) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: `File not supported. Expected formats: ${mimeTypes
+          .map((val: string) => `"${val}"`)
+          .join(", ")}.`,
+        fatal: true,
+      });
+
+      return z.NEVER;
+    }
+
+    if (val.size > maxSize) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: `The file size should not exceed ${
+          Math.floor(maxSize) / 1000000
+        }Mo.`,
+        fatal: true,
+      });
+
+      return z.NEVER;
+    }
+
+    return {
+      file: new File(
+        [val],
+        formatFilename({
+          filename: val.name,
+          mimeType: fileTypeResult.mime,
+        }),
+        {
+          type: fileTypeResult.mime,
+          lastModified: val.lastModified,
+        },
+      ),
+      buffer: Buffer.from(arrayBuffer),
+    };
+  });
