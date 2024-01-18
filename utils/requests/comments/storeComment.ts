3aced19c64eb1c8e1af3ada45e40672c46254789
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-01-18 18:24:17 +0300

    refactor: :recycle: Use makeContentClientSchema for store-comment schema.

--- utils/requests/comments/storeComment.ts
+++ utils/requests/comments/storeComment.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 import { commentConfig } from "~/utils/configs";
-import { countHtmlAsTextLength } from "~/utils/strings";
-import { CustomNullSchema, UserSchema, CommentSchema } from "~/utils/schemas";
+import {
+  CustomNullSchema,
+  UserSchema,
+  CommentSchema,
+  makeContentClientSchema,
+} from "~/utils/schemas";
 
 /* -------------------------------------------------------------------------- */
 /*                             Store comment body                             */
@@ -14,40 +18,10 @@ export const StoreCommentBodyBaseSchema = z.object({
 
 export const StoreCommentBodyClientSchema = StoreCommentBodyBaseSchema.merge(
   z.object({
-    content: z
-      .string()
-      .trim()
-      .superRefine((value: string, ctx) => {
-        const parser = new DOMParser();
-        const document = parser.parseFromString(value, "text/html");
-        const body = document.body;
-
-        const length: number = countHtmlAsTextLength(body);
-
-        if (length < commentConfig.CONTENT_MIN_LENGTH) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.too_small,
-            minimum: commentConfig.CONTENT_MIN_LENGTH,
-            inclusive: true,
-            fatal: true,
-            type: "string",
-          });
-
-          return z.NEVER;
-        }
-
-        if (length > commentConfig.CONTENT_MAX_LENGTH) {
-          ctx.addIssue({
-            code: z.ZodIssueCode.too_big,
-            fatal: true,
-            maximum: commentConfig.CONTENT_MAX_LENGTH,
-            inclusive: true,
-            type: "string",
-          });
-
-          return z.NEVER;
-        }
-      }),
+    content: makeContentClientSchema(
+      commentConfig.CONTENT_MIN_LENGTH,
+      commentConfig.CONTENT_MAX_LENGTH,
+    ),
   }),
 );
 
