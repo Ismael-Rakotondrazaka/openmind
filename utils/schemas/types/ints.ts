754c0fb210dafb3af38dfbe6e5f5f7b696feaa5c
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-01-18 17:20:29 +0300

    feat: :sparkles: Add more int schemas.

--- utils/schemas/types/ints.ts
+++ utils/schemas/types/ints.ts
@@ -27,3 +27,33 @@ export const IntFilterSchema: z.ZodType<Prisma.IntFilter> = z.object({
     .union([z.coerce.number(), z.lazy(() => NestedIntFilterSchema)])
     .optional(),
 });
+
+export const IntNullableFilterSchema: z.ZodType<Prisma.IntNullableFilter> =
+  z.object({
+    equals: z.number().optional().nullable(),
+    in: z.number().array().optional().nullable(),
+    notIn: z.number().array().optional().nullable(),
+    lt: z.number().optional(),
+    lte: z.number().optional(),
+    gt: z.number().optional(),
+    gte: z.number().optional(),
+    not: z
+      .union([z.number(), z.lazy(() => NestedIntNullableFilterSchema)])
+      .optional()
+      .nullable(),
+  });
+
+export const NestedIntNullableFilterSchema: z.ZodType<Prisma.NestedIntNullableFilter> =
+  z.object({
+    equals: z.number().optional().nullable(),
+    in: z.number().array().optional().nullable(),
+    notIn: z.number().array().optional().nullable(),
+    lt: z.number().optional(),
+    lte: z.number().optional(),
+    gt: z.number().optional(),
+    gte: z.number().optional(),
+    not: z
+      .union([z.number(), z.lazy(() => NestedIntNullableFilterSchema)])
+      .optional()
+      .nullable(),
+  });
