a323289cfc1381cafcf06f2ace2012cc955db360
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-01-26 11:22:20 +0300

    feat: :sparkles: Create user profile config.

---
+++ utils/configs/userProfileConfig.ts
@@ -0,0 +1,10 @@
+export const userProfileConfig = {
+  MAX_SIZE: 3_000_000, // 3Mb
+  MIME_TYPES: [
+    "image/png",
+    "image/webp",
+    "image/jpg",
+    "image/jpeg",
+    "image/svg+xml",
+  ],
+};
