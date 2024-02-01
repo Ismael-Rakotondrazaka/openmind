82e846289658c54ac147ed3399a4600c5000d689
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-02-01 10:10:39 +0300

    fix: :bug: Add missing import in reaction.ts.

--- schemas/globalSchemas/reactions/reaction.ts
+++ schemas/globalSchemas/reactions/reaction.ts
@@ -1,15 +1,13 @@
 import { z } from "zod";
 import { UserSchema } from "~/schemas/globalSchemas/users";
-import { ReactionTypeSchema } from "~/schemas/globalSchemas/reactions/reactionType";
-import type { ReactionArticle } from "~/schemas/globalSchemas/reactions/reactionArticle";
-import type { ReactionComment } from "~/schemas/globalSchemas/reactions/reactionComment";
-
-export const ReactionBaseSchema = z.object({
-  type: ReactionTypeSchema,
-  id: z.number().int(),
-  createdAt: z.coerce.date(),
-  userId: z.number().int(),
-});
+import {
+  type ReactionArticle,
+  ReactionArticleSchema,
+} from "~/schemas/globalSchemas/reactions/reactionArticle";
+import {
+  type ReactionComment,
+  ReactionCommentSchema,
+} from "~/schemas/globalSchemas/reactions/reactionComment";
 
 export const ReactionSchema: z.ZodType<ReactionArticle | ReactionComment> =
   z.lazy(() => z.union([ReactionArticleSchema, ReactionCommentSchema]));
