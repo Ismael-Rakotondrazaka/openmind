bfc627db10b96f7999b800bfdccf42aa957f6fb8
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-02-15 18:13:04 +0300

    feat: :sparkles: Create ArticleSummaryInput component.

---
+++ components/articles/ArticleSummaryInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex gap-2 flex-col w-full">
+    <label for="summary">Summary</label>
+
+    <PrimeInputText
+      id="summary"
+      v-model="summary"
+      :class="{ 'p-invalid': haveError }"
+    />
+
+    <small id="email-or-isVisible-text-error" class="text-red-600">{{
+      errorMessage || "&nbsp;"
+    }}</small>
+  </div>
+</template>
+
+<script setup lang="ts">
+const summary = defineModel<string | null, string>("summary");
+
+const props = defineProps({
+  errorMessage: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+});
+
+const haveError = computed<boolean>(() => props.errorMessage !== undefined);
+</script>
