c2a72ae10e1a964429e5a2b39121e6ac056be668
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-02-15 18:10:31 +0300

    feat: :sparkles: Create ArticleContentInput component.

---
+++ components/articles/ArticleContentInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <label for="email">Content</label>
+
+    <PrimeEditor v-model="content" editor-style="height: 320px" />
+
+    <small id="email-text-error" class="text-red-600">{{
+      errorMessage || "&nbsp;"
+    }}</small>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const content = defineModel<string, string>("content");
+
+defineProps({
+  errorMessage: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+});
+</script>
