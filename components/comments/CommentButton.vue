af42e0d885ea931b819e9c9a04a26d6b469672ce
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-02-22 18:53:32 +0300

    feat: :sparkles: Create the CommentButton component.

---
+++ components/comments/CommentButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <PrimeButton
+    icon="pi pi-comment"
+    severity="secondary"
+    :label="formattedCount"
+    text
+  />
+</template>
+
+<script lang="ts" setup>
+const count = defineModel<number>("count", {
+  required: true,
+});
+
+const formattedCount = useNumericAbbreviation(count);
+</script>
