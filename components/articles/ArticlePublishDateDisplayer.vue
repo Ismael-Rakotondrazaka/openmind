90ba04a27d7b9f40eac4293317e42c631943c924
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-02-22 19:00:48 +0300

    feat: :sparkles: Create ArticlePublishDateDisplayer component.

---
+++ components/articles/ArticlePublishDateDisplayer.vue
@@ -0,0 +1,30 @@
+<template>
+  <p class="text-base text-gray-500 dark:text-gray-400">
+    <time
+      pubdate
+      :datetime="createdAtDateTimeFormatted"
+      :title="createdAtTitleFormatted"
+      >{{ createdAtTextFormatted }}</time
+    >
+  </p>
+</template>
+
+<script lang="ts" setup>
+interface ArticlePublishDateDisplayerProps {
+  date: Date;
+}
+
+const props = defineProps<ArticlePublishDateDisplayerProps>();
+
+const createdAtDateTimeFormatted = useDateFormat(
+  () => props.date,
+  "YYYY-MM-DD",
+);
+
+const createdAtTitleFormatted = useDateFormat(
+  () => props.date,
+  "MMMM Do, YYYY",
+);
+
+const createdAtTextFormatted = useDateFormat(() => props.date, "MMM. M, YYYY");
+</script>
