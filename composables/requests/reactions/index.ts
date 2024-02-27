fbe5b7afea5684a09c72c9fee40bd5775ed60429
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-02-27 17:04:07 +0300

    feat: :sparkles: Create a composable to compute reactions preview text.

--- composables/requests/reactions/index.ts
+++ composables/requests/reactions/index.ts
@@ -2,3 +2,4 @@ export * from "./useStoreReaction";
 export * from "./useDestroyReaction";
 export * from "./useUpdateReaction";
 export * from "./useIndexReaction";
+export * from "./useReactionPreviewText";
