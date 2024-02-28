e9e0a2b8b01511267dd0b035da2241fbd27c6ac2
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-02-28 17:29:21 +0300

    feat: :sparkles: Declare ArticleReactionSidebarToken injection token.

--- di/reactions/index.ts
+++ di/reactions/index.ts
@@ -6,3 +6,11 @@ export type IndexReactionDI = {
 export const IndexReactionToken = Symbol(
   "IndexReactionToken",
 ) as InjectionKey<IndexReactionDI>;
+
+export type ArticleReactionSidebarDI = {
+  isVisible: Ref<boolean>;
+};
+
+export const ArticleReactionSidebarToken = Symbol(
+  "ArticleReactionSidebar",
+) as InjectionKey<ArticleReactionSidebarDI>;
