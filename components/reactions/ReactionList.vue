da9d387721df6e9eae00e91df027908d1dfe9946
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-02-28 17:33:17 +0300

    feat: :sparkles: Create components for reaction list and items.

---
+++ components/reactions/ReactionList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li
+      v-for="reaction in reactions"
+      :key="reaction.id"
+      class="mb-3 last-of-type:mb-0"
+    >
+      <ReactionItem :reaction="reaction" />
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+interface ReactionListProps {
+  reactions: ReactionFull[];
+}
+
+defineProps<ReactionListProps>();
+</script>
