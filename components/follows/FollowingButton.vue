358c606906ad3ffa88711cff58b13c65275d5448
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-04-15 16:10:30 +0300

    fix: :bug: Add confirm dialog when unfollowing.

--- components/follows/FollowingButton.vue
+++ components/follows/FollowingButton.vue
@@ -4,11 +4,42 @@
     icon="pi pi-chevron-down"
     severity="secondary"
     outlined
-    @click="onDeleteFollowingHandler"
+    @click="onOpenOverlayHandler"
+  />
+
+  <PrimeOverlayPanel ref="overlayPanel">
+    <div class="flex flex-col gap-5">
+      <p class="text-text">
+        Followed since
+        <span class="font-bold">{{ formattedFollowingDate }}</span>
+      </p>
+
+      <PrimeButton
+        outlined
+        label="Unfollow"
+        severity="danger"
+        icon="pi pi-user-minus"
+        @click="onOpenDialogHandler"
+      />
+    </div>
+  </PrimeOverlayPanel>
+
+  <ConfirmDialog
+    v-model:visible="isDialogVisible"
+    :header="dialogHeader"
+    :message="dialogMessage"
+    :info-list="infoList"
+    resolve-button-label="Yes, Unfollow"
+    reject-button-label="Cancel"
+    severity="danger"
+    @dialog:resolved="onDeleteFollowingHandler"
+    @dialog:rejected="onDialogRejectedHandler"
   />
 </template>
 
 <script setup lang="ts">
+import type { PrimeOverlayPanel } from "#build/components";
+
 interface FollowingButtonProps {
   follow: FollowFull;
 }
@@ -22,6 +53,38 @@ type FollowingButtonEmits = {
 const emit = defineEmits<FollowingButtonEmits>();
 
 const toast = useToast();
+
+const overlayPanel = ref<InstanceType<typeof PrimeOverlayPanel>>();
+
+const isDialogVisible = ref<boolean>(false);
+const onOpenDialogHandler = () => {
+  isDialogVisible.value = true;
+};
+const onDialogRejectedHandler = () => {
+  isDialogVisible.value = false;
+};
+
+const onOpenOverlayHandler = (event: Event) => {
+  if (overlayPanel.value !== undefined) {
+    overlayPanel.value.toggle(event);
+  }
+};
+
+const dialogHeader = computed<string>(
+  () => `Unfollow ${props.follow.following.username}`,
+);
+const dialogMessage = computed<string>(
+  () => `Are you sure you want to unfollow ${props.follow.following.username}`,
+);
+const infoList = computed<string[]>(() => [
+  `You will see less of ${props.follow.following.username}'s articles.`,
+  "You still can visit this profile.",
+]);
+
+const formattedFollowingDate = useDateFormat(
+  () => props.follow.createdAt,
+  "MMMM, D YYYY",
+);
 
 const {
   follow,
@@ -52,5 +115,3 @@ const onDeleteFollowingHandler = async () => {
   }
 };
 </script>
-
-<style scoped></style>
