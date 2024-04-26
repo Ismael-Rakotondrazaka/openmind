71628ddcf8b61dd0e4ac151994705a74d6d78f28
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-04-26 12:57:51 +0300

    feat: :sparkles: Add search by tags to article search.

--- components/articles/ArticleSearch.vue
+++ components/articles/ArticleSearch.vue
@@ -1,21 +1,28 @@
 <template>
-  <form
-    action=""
-    method="post"
-    class="flex gap-3"
-    @submit.prevent="onSearchHandler"
-  >
-    <PrimeInputGroup>
+  <form action="" method="post" class="" @submit.prevent="onSearchHandler">
+    <PrimeInputGroup class="mb-3">
       <PrimeInputText v-model="titleSearch" placeholder="Search by title" />
       <PrimeButton icon="pi pi-search" type="submit" />
     </PrimeInputGroup>
 
-    <PrimeDropdown
-      v-model="selectedOption"
-      :options="selectOptions"
-      option-label="text"
-      class="w-full md:w-1/3"
-    />
+    <div class="flex w-full items-start gap-3">
+      <div class="w-1/3">
+        <InputLabel label-for="Type" label="Type" :is-required="false" />
+
+        <PrimeDropdown
+          v-model="selectedOption"
+          :options="selectOptions"
+          option-label="text"
+          class="w-full"
+        />
+      </div>
+
+      <TagInput
+        v-model:tags="tags"
+        :consider-new-tag="false"
+        :is-required="false"
+      />
+    </div>
   </form>
 </template>
 
@@ -23,6 +30,8 @@
 import type { Prisma } from "@prisma/client";
 
 const titleSearch = ref<string | null>();
+
+const tags = ref<Tag[]>([]);
 
 type SelectedOptionKey = "ALL" | "VIEWED";
 type SelectOption = {
@@ -64,6 +73,7 @@ const { user: authUser } = useAuthUser();
 const computeWhereNewValue = (
   authUserId: number | undefined,
   selectedOptionKey: SelectedOptionKey,
+  tags: Tag[],
 ): Prisma.ArticleWhereInput => {
   return {
     views:
@@ -81,20 +91,37 @@ const computeWhereNewValue = (
             mode: "insensitive",
           }
         : undefined,
+    tags:
+      tags.length > 0
+        ? {
+            some: {
+              id: {
+                in: tags.map((tag: Tag) => tag.id),
+              },
+            },
+          }
+        : undefined,
   };
 };
 
 const onSearchHandler = () => {
   emit(
     "where:update",
-    computeWhereNewValue(authUser.value?.id, selectedOption.value.key),
+    computeWhereNewValue(
+      authUser.value?.id,
+      selectedOption.value.key,
+      tags.value,
+    ),
   );
 };
 
 watch(
-  [() => authUser.value?.id, () => selectedOption.value.key],
-  ([authUserId, selectedOptionKey]) => {
-    emit("where:update", computeWhereNewValue(authUserId, selectedOptionKey));
+  [() => authUser.value?.id, () => selectedOption.value.key, tags],
+  ([authUserId, selectedOptionKey, tags]) => {
+    emit(
+      "where:update",
+      computeWhereNewValue(authUserId, selectedOptionKey, tags),
+    );
   },
 );
 </script>
