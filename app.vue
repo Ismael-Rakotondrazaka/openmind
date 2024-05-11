e684f6edecedd03bd8c024de5dac6172fc831c01
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-05-11 16:48:40 +0300

    feat: :lipstick: Add transition per page navigation.

--- app.vue
+++ app.vue
@@ -3,3 +3,32 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script lang="ts" setup></script>
+
+<style>
+html {
+  scroll-behavior: smooth;
+}
+.page-leave-from {
+  opacity: 0.5;
+}
+.page-leave-to {
+  opacity: 0;
+}
+.page-enter-from {
+  opacity: 0;
+}
+.page-enter-to {
+  opacity: 1;
+}
+.page-leave-active {
+  transition: all 1s;
+  position: absolute;
+  inset: 0;
+  top: 12rem;
+}
+.page-enter-active {
+  transition: all 1s;
+}
+</style>
