c28a2325a3b04997ea69999dd0b2dd7088499720
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2024-05-28 15:39:13 +0300

    feat: :seedling: Create seeder for demo.

---
+++ prisma/seeders/prod/index.ts
@@ -0,0 +1,66 @@
+/* eslint-disable no-console */
+import { PrismaClient } from "@prisma/client";
+import { articlesData, uniqueTagsData } from "./data";
+import { createTags } from "./tags";
+import { createUsers } from "./users";
+
+import { type Tag, type User } from "@prisma/client";
+import { createArticles } from "./articles";
+
+const prismaClient = new PrismaClient();
+
+const main = async () => {
+  console.time("=> Total seed duration");
+
+  const simulationYears: number = 1;
+
+  /* ---------------------------------- Tags ---------------------------------- */
+  console.time("Tag seed duration");
+
+  const tags: Tag[] = await createTags({
+    prisma: prismaClient,
+    values: uniqueTagsData,
+  });
+
+  console.timeEnd("Tag seed duration");
+  /* -------------------------------------------------------------------------- */
+
+  console.time("User seed duration");
+
+  const users: User[] = await createUsers({
+    prisma: prismaClient,
+    years: simulationYears,
+    tags,
+  });
+
+  console.timeEnd("User seed duration");
+  /* -------------------------------------------------------------------------- */
+
+  /* --------------------------------- Article -------------------------------- */
+  console.time("Article seed duration");
+
+  await createArticles({
+    prisma: prismaClient,
+    users,
+    tags,
+    data: articlesData,
+  });
+
+  console.timeEnd("Article seed duration");
+  /* -------------------------------------------------------------------------- */
+
+  console.timeEnd("=> Total seed duration");
+};
+
+main()
+  .then(async () => {
+    await prismaClient.$disconnect();
+  })
+
+  .catch(async (e) => {
+    console.error(e);
+
+    await prismaClient.$disconnect();
+
+    process.exit(1);
+  });
